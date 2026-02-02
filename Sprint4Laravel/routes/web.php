23064a3c4d4f1d526198dbe8ac0b9e17caf28cc1
Lucas <lxcxsito@gmail.com>
2026-02-02 12:36:08 +0100

    Subida de la logica para loguear usuario, con su controlador , vista y ruta

--- Sprint4Laravel/routes/web.php
+++ Sprint4Laravel/routes/web.php
@@ -2,12 +2,16 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
 
     
 //User Routes GET
 Route::get('/register', action: [RegisterController::class, 'index']) -> name('register.index');
+Route::get('/login', action: [LoginController::class, 'index']) -> name('login.index');
+
 
 //User routes POST
 Route::post('/register', [RegisterController::class, 'register'])->name('register');
+Route::post('/login', [LoginController::class, 'login'])->name('login');
 
 ?>
